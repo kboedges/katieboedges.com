7d9f9630fea4b8a0a1bbcd83102642eac9ffc1d4
Katherine Boedges <kboedges@me.com>
2018-05-30 18:53:47 -0500

    Adding react and some basics

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="opening">Welcome.</h1>
+        <p className="about">I'm a QA and frontend developer located in Austin, Texas. I spend a lot of my free time learning about web and practicing my static trapeze skills. Astronomy is pretty cool too.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
