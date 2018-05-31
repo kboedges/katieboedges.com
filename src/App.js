84b110367f50dafed7ffbcbf3b98b79815ffd56a
Katherine Boedges <kboedges@me.com>
2018-05-30 19:51:35 -0500

    Adding About.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import About from './About'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="opening">Welcome.</h1>
-        <p className="about">I'm a QA and frontend developer located in Austin, Texas. I spend a lot of my free time learning about web and practicing my static trapeze skills. Astronomy is pretty cool too.</p>
+        <About />
       </div>
     );
   }
