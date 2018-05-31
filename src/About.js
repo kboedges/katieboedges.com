222a7d3062b82b2d28b180e50d4cfa0ed8bb25fc
Katherine Boedges <kboedges@me.com>
2018-05-31 11:06:49 -0500

    Adding SVG ground

--- src/About.js
+++ src/About.js
@@ -6,6 +6,9 @@ class About extends Component {
       <div className="About p-5 w-100">
         <h1 className="opening mb-4">Welcome.</h1>
         <p className="about-me">I'm a <span>QA Tech</span> and <span>frontend developer</span> located in <span>Austin, Texas</span>. I spend a lot of my free time learning about web development and practicing static trapeze. When I'm not doing one of those two things, I might be found at Austin's famous <span>uncommom OBJECTS</span> looking for antique photographs to add to my collection. I could also be sewing or cross-stitching...I have a lot of hobbies.</p>
+
+        
+  
       </div>
     );
   }
