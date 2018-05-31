b05c644884d2532a199230747cc86234ee8408c8
Katherine Boedges <kboedges@me.com>
2018-05-31 16:04:55 -0500

    Adjusting height of svg

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
           <Education/>
         )}/>
         
-        <svg className="hills" x="0" y="0" width="100%" height="500" viewBox="0 0 1400 500" xmlns="http://www.w3.org/2000/svg">
+        <svg className="hills" x="0" y="0" width="100%" height="auto" viewBox="0 0 1400 500" xmlns="http://www.w3.org/2000/svg">
           <rect x="0" y="460" width="1400" height="40" fill="black" />
           <path d="M0 460 C 100 460, 20 430, 200 470" stroke="black" fill="black"/>
           <path d="M200 470 C 260 450, 500 450, 800 460" stroke="black" fill="black"/>
