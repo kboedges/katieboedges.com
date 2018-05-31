68579ed9af5433f284202bee1c85ee650726a256
Katherine Boedges <kboedges@me.com>
2018-05-31 09:50:08 -0500

    Adjusting colors and adding information to Work.js

--- src/Card.js
+++ src/Card.js
@@ -4,26 +4,26 @@ import PropTypes from 'prop-types'
 class Card extends Component {
     static propTypes = {
         title: PropTypes.string.isRequired,
-        subtitle: PropTypes.string.isRequired,
+        subtitle: PropTypes.string,
         text: PropTypes.string.isRequired,
-        img: PropTypes.object.isRequired,
-        link: PropTypes.string.isRequired
+        img: PropTypes.object,
+        link: PropTypes.string.isRequired,
+        tech: PropTypes.string
     }
 
     render() {
-        const { title, subtitle, text, img, link } = this.props
+        const { title, subtitle, text, img, link, tech } = this.props
 
         return (
         <div className="card d-flex flex-row mb-3">
             <a href={link} className="card-link">
-            <img class="card-img" src={img} alt="apsu logo"/>
+            {img && (<img class="card-img" src={img} alt="logo"/>)}
             </a>
             <div className="card-body p-4">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-subtitle m-0">{subtitle}</p>
-                <p className="card-text m-0">
-                {text}
-                </p>
+                <p className="card-text m-0">{text}</p>
+                {tech && (<p className="card-tech mb-0 mt-2">{tech}</p>)}
             </div>
         </div>
         );
--- src/Work.js
+++ src/Work.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import Card from './Card'
 
 class Work extends Component {
   render() {
     return (
       <div className="work p-5">
+        <h1 className="opening mb-4">Work.</h1>
+        <p>Development projects that I've worked on</p>
+        <div className="d-flex flex-row flex-wrap">
+          <Card 
+            title={"Cyber Security Countdown"}
+            text={"2018 - Stop a ransomware attack by entering in 6 correct passwords within 5 hours."}
+            link={"https://github.com/kboedges/cybersec-timer"}
+            tech={"JavaScript, React, Redux"}
+          />
+      </div>
       </div>
     );
   }
