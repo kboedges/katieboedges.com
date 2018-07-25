374e921bd25493a6b32cd72615162ddf72519c72
Katherine Boedges <kboedges@me.com>
2018-07-25 17:18:10 -0500

    Adjusting color

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Nav extends Component {
   render() {
     return (
       <div className="nav px-5 py-1 w-100 d-flex flex-row justify-content-between">
-        <p className="nav-name mb-0 mt-1">Katie Boedges</p>
+        <Link className="nav-name mb-0 mt-1" to="/about">
+          Katie Boedges
+        </Link>
         <div className="nav-nav">
-            <ol className="d-flex flex-row justify-content-between list-unstyled mb-0">
-                <Link className="nav-item mt-1 current-page" to="/about">About</Link>
-                <Link className="nav-item ml-4 mt-1" to="/education">Education</Link>
-                <Link className="nav-item ml-4 mt-1" to="/work">Work</Link>
-                
-            </ol>
+          <ol className="d-flex flex-row justify-content-between list-unstyled mb-0">
+            <Link className="nav-item mt-1 current-page" to="/about">
+              About
+            </Link>
+            <Link className="nav-item ml-4 mt-1" to="/education">
+              Education
+            </Link>
+            <Link className="nav-item ml-4 mt-1" to="/work">
+              Work
+            </Link>
+          </ol>
         </div>
       </div>
     );
