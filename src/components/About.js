1b079e911c79cfeb182ae0a7c1fa8f2e2aa7c6a7
Katie Boedges <katie.boedges@mindbodyonline.com>
2019-03-27 11:28:11 -0700

    update about

--- src/components/About.js
+++ src/components/About.js
@@ -12,18 +12,23 @@ class About extends Component {
         <div className="about-info">
           <h1 className="opening mb-4">Welcome.</h1>
           <p className="about-me">
-            I'm a <span>QA technician</span> and <span>frontend developer</span> located in <span>Austin, Texas</span>.
-            I spend a lot of my free time learning about web development and practicing static trapeze. When I'm not
-            doing one of those two things, I might be found at Austin's famous <span>uncommom OBJECTS</span> looking for
-            antique photographs to add to my collection. I could also be sewing or cross-stitching...I have a lot of
-            hobbies.
+            I'm a <span>frontend software engineer</span> located in{" "}
+            <span>Austin, Texas</span>. I spend a lot of my free time learning
+            about web development and practicing static trapeze. When I'm not
+            doing one of those two things, I might be found at Austin's famous{" "}
+            <span>uncommom OBJECTS</span> looking for antique photographs to add
+            to my collection. I could also be sewing or cross-stitching...I have
+            a lot of hobbies.
           </p>
         </div>
         <div className="about-social">
           <a href="https://github.com/kboedges" target="_blank">
             <FontAwesomeIcon icon={["fab", "github"]} />
           </a>
-          <a href="https://www.linkedin.com/in/katherine-boedges-18baa339/" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/katherine-boedges-18baa339/"
+            target="_blank"
+          >
             <FontAwesomeIcon icon={["fab", "linkedin"]} />
           </a>
           <a href="https://codepen.io/kboedges/" target="_blank">
